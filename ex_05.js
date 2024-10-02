01046a29e0e38972686db5145236345d438be875
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-02 12:42:27 +0200

    tableau maison

---
+++ ex_05.js
@@ -0,0 +1,15 @@
+function getHousesPrototype() {
+    const maison = new Map();
+        
+        maison.set('Apartment 75008', 90);
+        maison.set('Apartment 94300', 105);
+        maison.set('Apartment 92300', 140);
+        maison.set('Apartment 75015', 65);
+        return maison;
+    }
+    
+console.log(getHousesPrototype());
+    
+//fonctionne sur vs code,pas sur jSfiddle 
+    
+    
