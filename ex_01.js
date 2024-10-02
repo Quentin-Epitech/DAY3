971ca03348e7cf4542c2ef6ed727e158ae7fe9ae
Quentin <quentin.bonnet@epitech.digital>
2024-10-02 16:48:32 +0200

    ex_01.js

--- ex_01.js
+++ ex_01.js
@@ -1,10 +1,10 @@
-
+// Your code here
 function getAngryDog(numberOfWoofs)
 {    let result = "";
 		 
     for (i = 0; i < numberOfWoofs; i++) result += "woof";
     return result;
-// Your code here
+
 }
 console.log("displayResult(getAngryDog(5))")
 
@@ -12,8 +12,8 @@ console.log("displayResult(getAngryDog(5))")
 
 
 
-
-//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
-
 var result = getAngryDog(5);
 displayResult(result);
+//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
+
+
