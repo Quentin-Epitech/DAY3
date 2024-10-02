e7bc9815e837fd62a08eaef7434de4dba509c58e
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-02 10:10:05 +0200

    Woof

---
+++ ex_01.js
@@ -0,0 +1,19 @@
+
+function getAngryDog(numberOfWoofs)
+{    let result = "";
+		 
+    for (i = 0; i < numberOfWoofs; i++) result += "woof";
+    return result;
+// Your code here
+}
+console.log("displayResult(getAngryDog(5))")
+
+
+
+
+
+
+//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
+
+var result = getAngryDog(5);
+displayResult(result);
